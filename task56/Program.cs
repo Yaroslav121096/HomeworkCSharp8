024549ad542885407aa3fb94b643673a711f89d5
Yaroslav <Valso96@mail.ru>
2022-12-16 18:26:42 +0600

    Строка с наименьшей суммой элементов

---
+++ task56/Program.cs
@@ -0,0 +1,56 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11);
+        }
+    }
+}
+int SumLine(int[,] matrix, int i)
+{
+    int sumLine = matrix[i, 0];
+    for (int j = 1; j < matrix.GetLength(1); j++)
+    {
+        sumLine += matrix[i, j];
+    }
+    return sumLine;
+}
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+void WriteArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Clear();
+int row = InputNumbers("Введите количество строк матрицы: ");
+int[,] matrix = new int[row, row + 1];
+InputMatrix(matrix);
+Console.Clear();
+Console.WriteLine($"Задана матрица размером: {row} на {row + 1}: ");
+Console.WriteLine($"\nИзначальный вид: ");
+WriteArray(matrix);
+int minSumLine = 0;
+int sumLine = SumLine(matrix, 0);
+for (int i = 1; i < matrix.GetLength(0); i++)
+{
+    int tempSumLine = SumLine(matrix, i);
+    if (sumLine > tempSumLine)
+    {
+        sumLine = tempSumLine;
+        minSumLine = i;
+    }
+}
+Console.WriteLine($"\n{minSumLine + 1} - строкa с наименьшей суммой элементов. Их сумма - {sumLine}\n");
