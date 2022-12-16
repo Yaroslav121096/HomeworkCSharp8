66f3e3f7adaa9f349af912059f72f35e6ba71de5
Yaroslav <Valso96@mail.ru>
2022-12-16 18:28:56 +0600

    Заполнение матрицы спиралью

---
+++ task62/Program.cs
@@ -0,0 +1,34 @@
+﻿void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      if (array[i,j] / 10 <= 0)
+      Console.Write($" {array[i,j]} ");
+
+      else Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+  }
+}
+Console.Clear();
+int[,] spiralMatrix = new int[4, 4];
+Console.WriteLine("Задан массив размером 4 х 4, заполненный спирально: \n");
+int temp = 1;
+int i = 0;
+int j = 0;
+while (temp <= spiralMatrix.GetLength(0) * spiralMatrix.GetLength(1))
+{
+    spiralMatrix[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < spiralMatrix.GetLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= spiralMatrix.GetLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > spiralMatrix.GetLength(1) - 1)
+        j--;
+    else
+        i--;
+}
+WriteArray(spiralMatrix);
